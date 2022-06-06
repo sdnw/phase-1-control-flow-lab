9514e6c8a2dbe8629cc6d9b1d02d5e3429d09ca3
Steven <stevendawn98@gmail.com>
2022-06-06 16:29:47 -0400

    commit tree 8ef21a0607f12e7c81176cadcba718dc6f61cf3a

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-function scuberGreetingForFeet(feet){
-  if (feet <= 400) {
+function scuberGreetingForFeet(feet) {
+  if (feet < 400) {
     return 'This one is on me!'
   } else if (feet > 400, feet <= 2000) {
-    return  'That will be twenty bucks.'
-  } else if (feet > 2000, feet <= 2500) {
+    return 'That will be twenty bucks.'
+  } else if (feet > 2000, feet < 2500) {
     return 'I will gladly take your thirty bucks.'
   } else if (feet > 2500) {
     return 'No can do.'
   }
-  
 }
 
-function ternaryCheckCity(city){
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+function ternaryCheckCity(city) {
+   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(tip){
-    switch(tip) {
-      case 'generous':
-        return 'Thank you so much.'
-      case 'not as generous':
-        return 'Thank you.'
-      case 'thanks for everything':
-        return 'Bye.'
-    }
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous': 
+    return 'Thank you so much.'
+
+    case 'not as generous':
+    return 'Thank you.'
+
+    default:
+    return 'Bye.'
+  }
 }
